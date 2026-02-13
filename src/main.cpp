20940b423e744269ceaf128542684cae8dd40cdd
Timothy Banks <timothyaaronbanks@gmail.com>
2026-02-12 23:17:51 -0500

    Flushing out Tendermint ABCI server

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,27 @@
+#include <boost/program_options.hpp>
 #include <charter/abci/server.hpp>
+#include <string>
 
-int main(int argc, char* argv[]) {}
+int main(int argc, char* argv[]) {
+  auto grpc_port = std::string{};
+
+  auto vm = boost::program_options::variables_map{};
+  auto description = boost::program_options::options_description{"Charter"};
+  description.add_options()("help,h", "Show the help message")(
+      "grpc-port,g",
+      boost::program_options::value<std::string>(&grpc_port)->default_value(
+          "0.0.0.0:50051"),
+      "IP:Port for the ABCI server")("verbose,v", "Enable verbose output");
+  boost::program_options::store(
+      boost::program_options::parse_command_line(argc, argv, description), vm);
+  boost::program_options::notify(vm);
+
+  if (vm.contains("help")) {
+    std::cout << description << std::endl;
+    return 0;
+  }
+
+  if (vm.contains("verbose")) {
+
+  }
+}
