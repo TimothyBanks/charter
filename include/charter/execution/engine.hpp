7933203a5f659bb9dd7c02380231201b659c7bea
Timothy Banks <timothyaaronbanks@gmail.com>
2026-02-27 20:02:45 -0500

    Code cleanup

--- include/charter/execution/engine.hpp
+++ include/charter/execution/engine.hpp
@@ -1,116 +1,27 @@
 #pragma once
 
+#include <charter/execution/app_info.hpp>
+#include <charter/execution/apply_snapshot_chunk_result.hpp>
+#include <charter/execution/block_result.hpp>
+#include <charter/execution/commit_result.hpp>
+#include <charter/execution/history_entry.hpp>
+#include <charter/execution/offer_snapshot_result.hpp>
+#include <charter/execution/query_result.hpp>
+#include <charter/execution/replay_result.hpp>
+#include <charter/execution/signature_verifier.hpp>
+#include <charter/execution/snapshot_descriptor.hpp>
+#include <charter/execution/tx_result.hpp>
 #include <charter/schema/primitives.hpp>
 #include <charter/schema/transaction.hpp>
 #include <charter/storage/rocksdb/storage.hpp>
 #include <cstdint>
-#include <functional>
 #include <mutex>
 #include <optional>
-#include <span>
 #include <string>
 #include <string_view>
 #include <vector>
 
 namespace charter::execution {
-
-struct tx_event_attribute final {
-  std::string key;
-  std::string value;
-  bool index{};
-};
-
-struct tx_event final {
-  std::string type;
-  std::vector<tx_event_attribute> attributes;
-};
-
-struct tx_result final {
-  uint32_t code{};
-  charter::schema::bytes_t data;
-  std::string log;
-  std::string info;
-  int64_t gas_wanted{};
-  int64_t gas_used{};
-  std::string codespace;
-  std::vector<tx_event> events;
-};
-
-struct block_result final {
-  std::vector<tx_result> tx_results;
-  charter::schema::hash32_t state_root;
-};
-
-struct commit_result final {
-  int64_t retain_height{};
-  int64_t committed_height{};
-  charter::schema::hash32_t state_root;
-};
-
-struct app_info final {
-  std::string data{"charter-custody"};
-  std::string version{"0.1.0-poc"};
-  uint64_t app_version{1};
-  int64_t last_block_height{};
-  charter::schema::hash32_t last_block_state_root;
-};
-
-struct query_result final {
-  uint32_t code{};
-  std::string log;
-  std::string info;
-  charter::schema::bytes_t key;
-  charter::schema::bytes_t value;
-  int64_t height{};
-  std::string codespace;
-};
-
-struct history_entry final {
-  uint64_t height{};
-  uint32_t index{};
-  uint32_t code{};
-  charter::schema::bytes_t tx;
-};
-
-struct replay_result final {
-  bool ok{};
-  uint64_t tx_count{};
-  uint64_t applied_count{};
-  int64_t last_height{};
-  charter::schema::hash32_t state_root;
-  std::string error;
-};
-
-using signature_verifier_t =
-    std::function<bool(const charter::schema::bytes_view_t& message,
-                       const charter::schema::signer_id_t& signer,
-                       const charter::schema::signature_t& signature)>;
-
-struct snapshot_descriptor final {
-  uint64_t height{};
-  uint32_t format{1};
-  uint32_t chunks{1};
-  charter::schema::hash32_t hash;
-  charter::schema::bytes_t metadata;
-};
-
-enum class offer_snapshot_result : uint8_t {
-  unknown = 0,
-  accept = 1,
-  abort = 2,
-  reject = 3,
-  reject_format = 4,
-  reject_sender = 5,
-};
-
-enum class apply_snapshot_chunk_result : uint8_t {
-  unknown = 0,
-  accept = 1,
-  abort = 2,
-  retry = 3,
-  retry_snapshot = 4,
-  reject_snapshot = 5,
-};
 
 class engine final {
  public:
